f2e1a463fe6a9c7ecce9e1080a1cdff340ea5ef7
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-04-19 12:05:46 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -14,20 +14,36 @@ class App extends Component {
   state = {
     score: 0,
     current: -1,
-    showGameOver: false
+    showGameOver: false,
+    pace: 1500,
+    rounds: 0,
+    gameOn: false,
     
   };
 
   timer = undefined;
 
      clickHandler = (i) => {
+
+      if (this.state.current !== i) {
+        this.stopHandler();
+
+        return;
+      }
+
       console.log("Button is clicked", i);
       this.setState({
-        score: this.state.score +10,
+        score: this.state.score + 10,
+        rounds: this.state.rounds - 1,
       });
     };
 
     nextCircle = () => {
+      if (this.state.rounds >= 1){
+        this.stopHandler();
+        return;
+      }
+
       let nextActive;
 
       do {
@@ -36,20 +52,25 @@ class App extends Component {
 
       this.setState({
         current: nextActive,
+        pace: this.state.pace * 0.95,
+        rounds: this.state.rounds + 1,
       });
-      
+      console.log('rounds', this.state.rounds);
       console.log("active circle:", this.state.current);
-      this.timer = setTimeout(this.nextCircle,1000);
+      this.timer = setTimeout(this.nextCircle,this.state.pace);
     }
 
     startHandler = () => {
       this.nextCircle();
-      };
+      this.setState({
+          gameOn: true
+      });
+    };
 
       stopHandler = () => {
         clearTimeout(this.timer);
         this.setState({
-          showGameOver: true
+          showGameOver: true, gameOn: false
         });
       };
 
@@ -65,23 +86,34 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
+      <div>
         <h1>Speedgame</h1>
         <p>Your score: {this.state.score}</p>
-        {circles.map((_,i)=> (
-          <Circle 
-              key={i} 
-              id={i} 
-              click={()=> this.clickHandler(i)} 
-              active={this.state.current === i}>
-
-          </Circle>
-        )
-          
+        <div className="circles">
+          {circles.map((_,i)=> 
+            (
+              <Circle 
+                  key={i} 
+                  id={i} 
+                  click={()=> this.clickHandler(i)} 
+                  active={this.state.current === i}
+                  disabled={this.state.gameOn}
+              />
+            )
+            
           )}
-        <Button click={this.startHandler}>START</Button>
-        <Button click={this.stopHandler}>END</Button>
-          {this.state.showGameOver && <Gameover click={this.closeHandler}/>}
+        </div>
+
+        <div>
+          <Button click={this.startHandler}>START</Button>
+          <Button click={this.stopHandler}>END</Button>
+        </div>
+          {this.state.showGameOver && (
+          <Gameover click={this.closeHandler} score={this.state.score}
+          
+          
+          />
+          )}
       </div>
     );
   }
--- src/Components/Circle.js
+++ src/Components/Circle.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Circle = (props) => {
     return (
-        <div className={`circle ${props.active ? 'active' : ''}`} onClick={props.click}>
+        <div 
+            style={{pointerEvents: props.disabled ? "auto": "none"}} 
+            className={`circle ${props.active ? 'active' : ''}`} 
+            onClick={props.click}>
             {props.id}
         </div>
     );
